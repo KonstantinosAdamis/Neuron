f4937653d256ab020321767017621f66679bc01f
Konstantinos Adamis <kon.adamis2@gmail.com>
2025-07-15 12:30:07 +0300

    Add files via upload

---
+++ neuron.py
@@ -0,0 +1,7 @@
+inputs = [1, 2, 1, 2]
+weights = [-1, 1, -1, 1]
+bias = 1
+
+outputs = inputs[0] * weights[0] + inputs[1] * weights[1] + inputs[2] * weights[2] + inputs[3] * weights[3] + bias
+
+print(outputs)
